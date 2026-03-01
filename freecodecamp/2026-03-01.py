f021c6ab1eeba4fd509dba6676e881aa9fe3e29b
Alpesh Khatri <4825904+alpeshkhatri@users.noreply.github.com>
2026-03-01 20:36:45 +0000

    2026-03-01

--- freecodecamp/2026-03-01.py
+++ freecodecamp/2026-03-01.py
@@ -13,18 +13,6 @@ def is_flat(args) -> bool:
     return True
 
 
-# javascript equivalent
-# function isFlat(args) {
-#   console.debug("start of isFlat", { args });
-#   for (const s of args) {
-#    if (Array.isArray(s)) {
-#      return false;
-#    }
-#   }
-#   return true;
-# }
-
-
 class TestCodeChallenge(unittest.TestCase):
     def test_daily_coding_challenge(self):
         self.assertEqual(is_flat([1, 2, 3, 4]), True)
@@ -37,3 +25,27 @@ class TestCodeChallenge(unittest.TestCase):
 if __name__ == "__main__":
 
     unittest.main(verbosity=2)
+
+# javascript equivalent
+# 
+# function isFlat(args) {
+#   console.debug(`start of is_flat args=${JSON.stringify(args)}`);
+#   for (const s of args) {
+#     if (Array.isArray(s)) return false;
+#   }
+#   return true;
+# }
+# 
+# // Tests
+# const tests = [
+#   [isFlat([1, 2, 3, 4]), true],
+#   [isFlat([1, [2, 3], 4]), false],
+#   [isFlat([1, 0, false, true, "a", "b"]), true],
+#   [isFlat(["a", [0], "b", true]), false],
+#   [isFlat([1, [2, [3, [4, [5]]]], 6]), false],
+# ];
+# 
+# tests.forEach(([result, expected], i) => {
+#   const pass = result === expected;
+#   console.log(`Test ${i + 1}: ${pass ? "PASS" : "FAIL"}`);
+# });
